702dfb35fdc63775ed06aeeb1aac80e8b21e6cb9
aarushi353 <aarushiabrol999@gmail.com>
2021-05-01 18:01:41 +0530

    edited js file

--- Dicee Challenge/index.js
+++ Dicee Challenge/index.js
@@ -5,9 +5,7 @@ var randomDiceImage = "dice" + randomNumber1 + ".png"; //dice1.png - dice6.png
 
 var randomImageSource = "images/" + randomDiceImage; //images/dice1.png - images/dice6.png
 
-var image1 = document.querySelectorAll("img")[0];
-
-image1.setAttribute("src", randomImageSource);
+document.querySelectorAll("img")[0].setAttribute("src", randomImageSource);
 
 
 var randomNumber2 = Math.floor(Math.random() * 6) + 1;
